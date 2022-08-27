cc57a52b34195cd798618513ddb5957add5fdacb
Chandrashekhar Nandanwar <shekhar15jan@gmail.com>
2022-08-27 12:56:40 +0530

    CTNSBM-01. Spring boot lesson one initial commit

---
+++ src/test/java/com/training/lessonone/LessononeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.lessonone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessononeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
